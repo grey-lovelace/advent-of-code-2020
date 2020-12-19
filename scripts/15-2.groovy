eaedf38414162f4edc3c3f93ef8e17e76ef09555
grey-lovelace <austin.grey.lovelace@gmail.com>
2020-12-19 14:05:34 -0600

    days 13.2 - 16.1

---
+++ scripts/15-2.groovy
@@ -0,0 +1,15 @@
+def data = (new File('../resources/day15.txt') as String[])[0].split(",").collect{it as Integer}
+def cache = [:]
+data.eachWithIndex{it, i -> cache[it] = i}
+def currentNum = 0
+def lastNum = data.last()
+cache.remove(lastNum)
+(data.size()..29999999).each{
+    currentNum = cache.containsKey(lastNum) ? it-cache[lastNum]-1 : 0
+    println "$it - $currentNum"
+    cache[lastNum] = it-1
+    lastNum = currentNum
+}
+println "Answer is ${currentNum}"
+
+// Works, but still not performant. Should take about 2 hours to run.
---
+++ scripts/16-1.groovy
@@ -0,0 +1,15 @@
+def data = (new File('../resources/day16.txt') as String[]).toList()
+def rulePattern = /.*?:\s+(\d+)\-(\d+) or (\d+)\-(\d+)/
+def validNums = []
+data[0..(data.indexOf("your ticket:")-2)].each{
+    println it
+    def matcher = it =~ rulePattern
+    matcher.find()
+    validNums += (matcher.group(1) as Integer)..(matcher.group(2) as Integer)
+    validNums += (matcher.group(3) as Integer)..(matcher.group(4) as Integer)
+    validNums = validNums.unique()
+}
+def answerArray = data[(data.indexOf("nearby tickets:")+1)..(data.size()-1)].collect{
+    it.split(",").collect{field -> field as Integer}.findAll{field -> !validNums.contains(field)}.sum()
+}.findAll{it!=null}
+println "Answer is ${answerArray.sum()}"
